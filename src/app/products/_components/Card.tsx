88c3b942423e98daf1e1f36ca179d82d495cb8a4
Samarajya Shrestha <samarajyastha@gmail.com>
2024-07-09 20:14:13 +0545

    feat: design products card

--- src/app/products/_components/Card.tsx
+++ src/app/products/_components/Card.tsx
@@ -1,15 +1,23 @@
 import { Product } from "@/types/product";
 import React from "react";
+import headphone from "@/assets/images/headphone.jpg";
+import Image from "next/image";
 
 const ProductCard = ({ product }: { product: Product }) => {
   if (!product?.name) throw new Error("Product not found!");
 
   return (
-    <div className="px-8 py-5 rounded-2xl shadow bg-slate-100 h-full">
-      <p>Name: {product?.name}</p>
-      <p>Brand: {product?.brand}</p>
-      <p>Category: {product?.category}</p>
-      <p>Price: {product?.price}</p>
+    <div className="py-5 h-full border border-dashed border-gray-800">
+      <div className="flex justify-center mb-5">
+        <Image src={headphone} alt="" height={200} className="" />
+      </div>
+      <p className="text-center font-semibold text-lg">$ {product?.price}</p>
+      <p className="text-center font-semibold text-xl">{product?.name}</p>
+      <p className="text-center">{product?.brand}</p>
+      <p className="text-center">{product?.category}</p>
+      <div className="text-center mt-3">
+        <button className="bg-black text-white px-3 py-1 ">Buy Now</button>
+      </div>
     </div>
   );
 };
