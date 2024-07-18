883510bfd6939d3fadd95a981712cde84d166ecd
Samarajya Shrestha <samarajyastha@gmail.com>
2024-07-18 20:20:52 +0545

    chore: fixed footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-black py-4">
+    <footer className="bg-black py-4 fixed bottom-0 w-full z-20">
       <div className="container mx-auto">
         <div className="text-center text-white">
           © Copyright 2024. All rights reserved.
